904e03905900634101a6cb9e9a7d5a09387bed91
goda-kazuki <goda@eyemovic.com>
2022-12-14 16:46:24 +0900

    自作のconstructを作成し、利用したところまで

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -1,21 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import {HitCounter} from "./hitcounter";
+import {TableViewer} from "cdk-dynamo-table-viewer";
+
 
 export class AwsCdkStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-
-        console.log(lambda.Code.fromAsset('lambda'));
 
         const helloLambda = new lambda.Function(this, 'HelloHandler', {
             runtime: lambda.Runtime.NODEJS_14_X,
             code: lambda.Code.fromAsset('lambda'),
             handler: 'hello.handler'
         });
-        const sampleApiGateway = new apigw.LambdaRestApi(this, 'Endpoint', {
-            handler: helloLambda
+
+        const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+            downstream: helloLambda
         })
-        console.log(sampleApiGateway);
+
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: helloWithCounter.handler
+        });
+
+        new TableViewer(this, 'ViewHitCounter', {
+            title: 'Hello Hits',
+            table: helloWithCounter.table,
+            sortBy: '-hits'
+        })
+
+
+        console.log(lambda.Code.fromAsset('lambda'));
     }
 }
